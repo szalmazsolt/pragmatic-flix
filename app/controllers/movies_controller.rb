48ecb7f19716ac7ffe3a510b7ee21887c4f895b3
Zsolt Szalma <zsolt.szalma@gmail.com>
2025-03-25 21:52:17 +0100

    Add show action, show view and route

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,11 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def show
+    @movie = Movie.find(params[:id])
+  rescue => err
+    puts "Error: #{err.message}"
+    redirect_to movies_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "movies", to: "movies#index"
+  get "movies/:id", to: "movies#show"
 end
