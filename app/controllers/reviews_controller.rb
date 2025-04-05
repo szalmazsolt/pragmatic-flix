6fbeea56421831fb4d7b8e45aee3a39b75ed1da1
szalmazsolt <zsolt.szalma@gmail.com>
2025-04-05 17:18:19 +0200

    Implement new and create actions for Review with a web form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,35 @@
 class ReviewsController < ApplicationController
+  before_action :set_movie, only: [ :index, :new, :create ]
+
+  # GET /movies/:movie_id/reviews
+  # GET /movies/:movie_id/reviews.json
+  # GET /movies/:movie_id/reviews.xml
+
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
+  end
+
+  def new
+    @review = @movie.reviews.new
+  end
+
+  def create
+    @review = @movie.reviews.new(review_params)
+
+    if @review.save
+      redirect_to movie_reviews_url(@movie), notice: "Thank you for your review!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:name, :comment, :stars)
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
   end
 end
