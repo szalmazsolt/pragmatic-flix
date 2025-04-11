f6eb6429012d0c5ab3e9e648f131e8f652afbe73
Zsolt Szalma <zsolt.szalma@gmail.com>
2025-04-11 12:00:37 +0200

    Complete sign in feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    fail
+    user = User.find_by(email: params[:email_or_username]) ||
+           User.find_by(username: params[:email_or_username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back, #{user.name}!"
+    else
+      flash.now[:alert] = "Invalid email or password. Try again!"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
